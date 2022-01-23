115c0f64bda7511865178d07b5b847747b54c291
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 <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>
2022-01-23 13:08:42 +0800

    新增加農砲物件腳本

---
+++ Assets/Frankxu/Scripts/CannonObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CannonObject : MonoBehaviour
+{
+    public float Speed = 3;
+
+    void Update()
+    {
+        transform.Translate(transform.forward * Time.deltaTime * Speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.tag == "Board")
+        {
+            collision.gameObject.SetActive(false);
+            transform.gameObject.SetActive(false);
+        }
+        else if(collision.transform.tag == "Black")
+        {
+            transform.gameObject.SetActive(false);
+        }
+    }
+}
